af00ec47943551d107fe7b5311dae7836907a830
dayux <paulcoignac@gmail.com>
2023-12-11 14:07:22 +0100

    test

--- backend/routers/dependencies.py
+++ backend/routers/dependencies.py
@@ -9,10 +9,10 @@ from bson import ObjectId
 
 client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGODB_URL"))
 db = client.dlisfy
-MONGO_URL=os.getenv("MONGO_URL")
-SECRET_KEY=os.getenv("SECRET_KEY")
-ALGORITHM=os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+MONGO_URL="mongodb://localhost:27017/dlisfy"
+SECRET_KEY="09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM="HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES=1440
 
 async def isAdmin(x_token: str = Header("X-Token")):
     credentials_exception = HTTPException(
