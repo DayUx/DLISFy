b23be85dc5d87ab1ae62a0057255a04676b9f2f7
dayux <paulcoignac@gmail.com>
2023-12-11 16:42:53 +0100

    Change ports

--- backend/routers/dependencies.py
+++ backend/routers/dependencies.py
@@ -9,7 +9,7 @@ from bson import ObjectId
 
 client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGODB_URL"))
 db = client.dlisfy
-MONGO_URL="mongodb://localhost:27017/dlisfy"
+MONGO_URL="mongodb+srv://paul:Password123@puntify-mongo.mongocluster.cosmos.azure.com/dlisfy?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
 SECRET_KEY="09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM="HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES=1440
--- frontend/src/utils/API.jsx
+++ frontend/src/utils/API.jsx
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/";
+const API_URL = "https://puntify-back.azurewebsites.net/";
 export const API = {
   login: API_URL + "auth/login",
   register: API_URL + "auth/register",
