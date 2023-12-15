f3771bff01e9d781adb814c7c07aa364db8c5496
dayux <paulcoignac@gmail.com>
2023-12-15 11:39:34 +0100

    start creating tf for aws

---
+++ terraform-aws/main.tf
@@ -0,0 +1,301 @@
+provider "aws" {
+  region     = "eu-north-1"
+
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags       = {
+    Name = "MyVPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  tags       = {
+    Name = "PublicSubnet1a"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1b" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.2.0/24"
+  tags       = {
+    Name = "PublicSubnet1b"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.3.0/24"
+  tags       = {
+    Name = "PrivateSubnet1a"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1b" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.4.0/24"
+  tags       = {
+    Name = "PrivateSubnet1b"
+  }
+}
+
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.myvpc.id
+  tags   = {
+    Name = "MyInternetGateway"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.myvpc.id
+  tags   = {
+    Name = "PublicRouteTable"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myigw.id
+  }
+
+  //route target local
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+}
+
+resource "aws_route_table_association" "public_route_table_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_route_table_association_2" {
+  subnet_id      = aws_subnet.public_subnet_1b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_eip" "myeip" {
+  vpc  = true
+  tags = {
+    Name = "MyEIP"
+  }
+}
+
+
+resource "aws_nat_gateway" "myng" {
+  allocation_id = aws_eip.myeip.id
+  subnet_id     = aws_subnet.private_subnet_1a.id
+  tags          = {
+    Name = "MyNatGateway"
+  }
+  depends_on = [aws_internet_gateway.myigw]
+}
+
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.myvpc.id
+  tags   = {
+    Name = "PrivateRouteTable"
+  }
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.myng.id
+  }
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+}
+
+
+
+
+
+
+
+
+
+resource "aws_route_table_association" "private_route_table_association_1" {
+  subnet_id      = aws_subnet.private_subnet_1a.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_route_table_association_2" {
+  subnet_id      = aws_subnet.private_subnet_1b.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+
+resource "aws_lb" "myalb" {
+  name               = "MyALB"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_subnet_1a.id, aws_subnet.public_subnet_1b.id]
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_target_group" "mytg" {
+  name     = "MyTG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+}
+
+resource "aws_lb_listener" "mylistener" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    target_group_arn = aws_lb_target_group.mytg.arn
+    type             = "forward"
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name        = "MyALBSecurityGroup"
+  description = "Allow inbound traffic to the ALB"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "bastion" {
+  name        = "MyBastionSecurityGroup"
+  description = "Allow inbound traffic to the Bastion"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    #    type        = "http"
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    #    type        = "https"
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  //egress accept all
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+#resource "aws_instance" "bastion" {
+#  ami           = "ami-0416c18e75bd69567"
+#  instance_type = "t2.micro"
+#  subnet_id = aws_subnet.public_subnet_1a.id
+#  tags = {
+#    Name = "Bastion"
+#  }
+#
+#  security_groups = [aws_security_group.bastion.id]
+#}
+
+resource "aws_instance" "app1" {
+  ami                    = "ami-0416c18e75bd69567"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.private_subnet_1a.id
+  vpc_security_group_ids = [aws_security_group.bastion.id]
+
+  tags = {
+    Name = "App1"
+  }
+
+  depends_on = [
+    aws_nat_gateway.myng,
+    aws_route_table.private_route_table
+  ]
+
+
+  user_data = <<-EOF
+#!/bin/bash
+sudo dnf update
+sudo dnf install -y docker git
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+sudo systemctl start docker.service
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo usermod -a -G docker ec2-user
+newgrp docker
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Clone the Git repository
+git clone https://github.com/DayUx/puntify.git
+
+# Change to the cloned directory
+cd puntify
+
+# Build and start the Docker Compose services
+docker-compose up -d
+EOF
+
+}
+
+resource "aws_instance" "app2" {
+  ami                    = "ami-0416c18e75bd69567"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.private_subnet_1b.id
+  vpc_security_group_ids = [aws_security_group.bastion.id]
+  tags                   = {
+    Name = "App2"
+  }
+
+  depends_on = [
+    aws_nat_gateway.myng,
+    aws_route_table.private_route_table
+  ]
+
+
+
+
+  user_data = <<-EOF
+#!/bin/bash
+sudo dnf update
+sudo dnf install -y docker git
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+sudo systemctl start docker.service
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo usermod -a -G docker ec2-user
+newgrp docker
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/DayUx/puntify.git
+cd puntify
+docker-compose up -d
+EOF
+
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,283 +1,284 @@
-provider "azurerm" {
-  features {}
-  subscription_id            = var.subscription_id
-  tenant_id                  = var.tenant_id
-  client_id                  = var.client_id
-  client_secret              = var.client_secret
-  skip_provider_registration = true
-}
 
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "puntifyeurope" {
-  name     = "puntifyeurope"
-  location = "westeurope"
-}
-
-resource "azurerm_resource_group" "puntifyus" {
-  name     = "puntifyus"
-  location = "eastus"
-}
-
-#resource "azurerm_app_service_plan" "puntifyeurope" {
-#  name                = "puntifyeurope-asp"
-#  resource_group_name = azurerm_resource_group.puntifyeurope.name
-#  location            = azurerm_resource_group.puntifyeurope.location
-#  kind                = "Linux"
-#  reserved            = true
-#  sku {
-#    tier = "Basic"
-#    size = "B1"
+#provider "azurerm" {
+#  features {}
+#  subscription_id            = var.subscription_id
+#  tenant_id                  = var.tenant_id
+#  client_id                  = var.client_id
+#  client_secret              = var.client_secret
+#  skip_provider_registration = true
+#}
+#
+#data "azurerm_client_config" "current" {}
+#
+#resource "azurerm_resource_group" "puntifyeurope" {
+#  name     = "puntifyeurope"
+#  location = "westeurope"
+#}
+#
+#resource "azurerm_resource_group" "puntifyus" {
+#  name     = "puntifyus"
+#  location = "eastus"
+#}
+#
+##resource "azurerm_app_service_plan" "puntifyeurope" {
+##  name                = "puntifyeurope-asp"
+##  resource_group_name = azurerm_resource_group.puntifyeurope.name
+##  location            = azurerm_resource_group.puntifyeurope.location
+##  kind                = "Linux"
+##  reserved            = true
+##  sku {
+##    tier = "Basic"
+##    size = "B1"
+##  }
+##}
+#locals {
+#  env_variables_europe = {
+#    DOCKER_REGISTRY_SERVER_URL      = "acrpuntify.azurecr.io"
+#    DOCKER_REGISTRY_SERVER_USERNAME = "acrpuntify"
+#    DOCKER_REGISTRY_SERVER_PASSWORD = "ayc0c2LrKJvA3Lfw8oEjGVWzSKxcpyfYh+Zmx84Wn5+ACRBgaxAW"
+#    MONGO_URL                       = azurerm_cosmosdb_account.puntifyeuropemongoaccount.connection_strings[0]
+#    SECRET_KEY                      = var.secret_key
+#    ALGORITHM                       = var.algorithm
+#    ACCESS_TOKEN_EXPIRE_MINUTES     = var.access_token_expire_minutes
 #  }
 #}
-locals {
-  env_variables_europe = {
-    DOCKER_REGISTRY_SERVER_URL      = "acrpuntify.azurecr.io"
-    DOCKER_REGISTRY_SERVER_USERNAME = "acrpuntify"
-    DOCKER_REGISTRY_SERVER_PASSWORD = "ayc0c2LrKJvA3Lfw8oEjGVWzSKxcpyfYh+Zmx84Wn5+ACRBgaxAW"
-    MONGO_URL                       = azurerm_cosmosdb_account.puntifyeuropemongoaccount.connection_strings[0]
-    SECRET_KEY                      = var.secret_key
-    ALGORITHM                       = var.algorithm
-    ACCESS_TOKEN_EXPIRE_MINUTES     = var.access_token_expire_minutes
-  }
-}
-
-
-resource "azurerm_cosmosdb_account" "puntifyeuropemongoaccount" {
-  name                = "puntify-europe-mongo"
-  resource_group_name = azurerm_resource_group.puntifyeurope.name
-  location            = azurerm_resource_group.puntifyeurope.location
-  offer_type          = "Standard"
-  kind                = "MongoDB"
-
-  enable_automatic_failover = true
-
-  capabilities {
-    name = "EnableAggregationPipeline"
-  }
-
-  capabilities {
-    name = "mongoEnableDocLevelTTL"
-  }
-
-  capabilities {
-    name = "MongoDBv3.4"
-  }
-
-  capabilities {
-    name = "EnableMongo"
-  }
-
-  consistency_policy {
-    consistency_level       = "BoundedStaleness"
-    max_interval_in_seconds = 300
-    max_staleness_prefix    = 100000
-  }
-  geo_location {
-    failover_priority = 0
-    location          = azurerm_resource_group.puntifyeurope.location
-  }
-}
-
-resource "azurerm_cosmosdb_mongo_database" "puntifyeuropemongodb" {
-  name                = "puntify-europe-mongo-db"
-  resource_group_name = azurerm_cosmosdb_account.puntifyeuropemongoaccount.resource_group_name
-  account_name        = azurerm_cosmosdb_account.puntifyeuropemongoaccount.name
-  throughput          = 3000
-}
-
 #
-#resource "azurerm_app_service" "puntifybackeurope" {
-#  name                    = "puntify-back-europe"
-#  location                = azurerm_resource_group.puntifyeurope.location
-#  resource_group_name     = azurerm_resource_group.puntifyeurope.name
-#  app_service_plan_id     = azurerm_app_service_plan.puntifyeurope.id
-#  https_only              = true
-#  client_affinity_enabled = true
-#  site_config {
-#    scm_type          = "VSTSRM"
-#    always_on         = true
-#    health_check_path = "/health"
-#    # health check required in order that internal app service plan loadbalancer do not loadbalance on instance down
-#    linux_fx_version  = "DOCKER|acrpuntify.azurecr.io/puntify-api:latest"
+#
+#resource "azurerm_cosmosdb_account" "puntifyeuropemongoaccount" {
+#  name                = "puntify-europe-mongo"
+#  resource_group_name = azurerm_resource_group.puntifyeurope.name
+#  location            = azurerm_resource_group.puntifyeurope.location
+#  offer_type          = "Standard"
+#  kind                = "MongoDB"
+#
+#  enable_automatic_failover = true
+#
+#  capabilities {
+#    name = "EnableAggregationPipeline"
 #  }
 #
-#  logs {
-#    http_logs {
-#      file_system {
-#        retention_in_days = 30
-#        retention_in_mb   = 30
+#  capabilities {
+#    name = "mongoEnableDocLevelTTL"
+#  }
+#
+#  capabilities {
+#    name = "MongoDBv3.4"
+#  }
+#
+#  capabilities {
+#    name = "EnableMongo"
+#  }
+#
+#  consistency_policy {
+#    consistency_level       = "BoundedStaleness"
+#    max_interval_in_seconds = 300
+#    max_staleness_prefix    = 100000
+#  }
+#  geo_location {
+#    failover_priority = 0
+#    location          = azurerm_resource_group.puntifyeurope.location
+#  }
+#}
+#
+#resource "azurerm_cosmosdb_mongo_database" "puntifyeuropemongodb" {
+#  name                = "puntify-europe-mongo-db"
+#  resource_group_name = azurerm_cosmosdb_account.puntifyeuropemongoaccount.resource_group_name
+#  account_name        = azurerm_cosmosdb_account.puntifyeuropemongoaccount.name
+#  throughput          = 3000
+#}
+#
+##
+##resource "azurerm_app_service" "puntifybackeurope" {
+##  name                    = "puntify-back-europe"
+##  location                = azurerm_resource_group.puntifyeurope.location
+##  resource_group_name     = azurerm_resource_group.puntifyeurope.name
+##  app_service_plan_id     = azurerm_app_service_plan.puntifyeurope.id
+##  https_only              = true
+##  client_affinity_enabled = true
+##  site_config {
+##    scm_type          = "VSTSRM"
+##    always_on         = true
+##    health_check_path = "/health"
+##    # health check required in order that internal app service plan loadbalancer do not loadbalance on instance down
+##    linux_fx_version  = "DOCKER|acrpuntify.azurecr.io/puntify-api:latest"
+##  }
+##
+##  logs {
+##    http_logs {
+##      file_system {
+##        retention_in_days = 30
+##        retention_in_mb   = 30
+##      }
+##    }
+##  }
+##
+##  app_settings = local.env_variables_europe
+##}
+#
+#
+#resource "azurerm_log_analytics_workspace" "puntifyeuropeloganalytics" {
+#  name                = "puntify-europe-log-analytics"
+#  location            = azurerm_resource_group.puntifyeurope.location
+#  resource_group_name = azurerm_resource_group.puntifyeurope.name
+#  sku                 = "PerGB2018"
+#  retention_in_days   = 30
+#}
+#
+#resource "azurerm_container_app_environment" "puntifyeuropeappenvironment" {
+#  name                       = "puntify-europe-app-environment"
+#  location                   = azurerm_resource_group.puntifyeurope.location
+#  resource_group_name        = azurerm_resource_group.puntifyeurope.name
+#  log_analytics_workspace_id = azurerm_log_analytics_workspace.puntifyeuropeloganalytics.id
+#}
+#
+#resource "azurerm_container_app" "puntifybackeurope" {
+#  name                         = "puntify-back-europe"
+#  container_app_environment_id = azurerm_container_app_environment.puntifyeuropeappenvironment.id
+#  resource_group_name          = azurerm_resource_group.puntifyeurope.name
+#  revision_mode                = "Single"
+#  template {
+#    container {
+#      name   = "puntify-back-europe-container"
+#      image  = "acrpuntify.azurecr.io/puntify-api:latest"
+#      cpu    = 0.25
+#      memory = "0.5Gi"
+#      env {
+#        name  = "MONGO_URL"
+#        value = azurerm_cosmosdb_account.puntifyeuropemongoaccount.connection_strings[0]
+#      }
+#
+#      env {
+#        name  = "SECRET_KEY"
+#        value = local.env_variables_europe.SECRET_KEY
+#      }
+#
+#      env {
+#        name  = "ALGORITHM"
+#        value = local.env_variables_europe.ALGORITHM
+#      }
+#      env {
+#        name  = "ACCESS_TOKEN_EXPIRE_MINUTES"
+#        value = local.env_variables_europe.ACCESS_TOKEN_EXPIRE_MINUTES
+#
 #      }
 #    }
 #  }
+#  ingress {
+#    allow_insecure_connections = false
+#    external_enabled           = true
+#    target_port                = 443
+#    traffic_weight {
+#      latest_revision = true
+#      percentage      = 100
+#    }
+#  }
+#  registry {
+#    server               = local.env_variables_europe.DOCKER_REGISTRY_SERVER_URL
+#    username             = local.env_variables_europe.DOCKER_REGISTRY_SERVER_USERNAME
+#    password_secret_name = "docker-registry-secret"
+#  }
 #
-#  app_settings = local.env_variables_europe
+#  secret {
+#    name  = "docker-registry-secret"
+#    value = local.env_variables_europe.DOCKER_REGISTRY_SERVER_PASSWORD
+#  }
 #}
-
-
-resource "azurerm_log_analytics_workspace" "puntifyeuropeloganalytics" {
-  name                = "puntify-europe-log-analytics"
-  location            = azurerm_resource_group.puntifyeurope.location
-  resource_group_name = azurerm_resource_group.puntifyeurope.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
-
-resource "azurerm_container_app_environment" "puntifyeuropeappenvironment" {
-  name                       = "puntify-europe-app-environment"
-  location                   = azurerm_resource_group.puntifyeurope.location
-  resource_group_name        = azurerm_resource_group.puntifyeurope.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.puntifyeuropeloganalytics.id
-}
-
-resource "azurerm_container_app" "puntifybackeurope" {
-  name                         = "puntify-back-europe"
-  container_app_environment_id = azurerm_container_app_environment.puntifyeuropeappenvironment.id
-  resource_group_name          = azurerm_resource_group.puntifyeurope.name
-  revision_mode                = "Single"
-  template {
-    container {
-      name   = "puntify-back-europe-container"
-      image  = "acrpuntify.azurecr.io/puntify-api:latest"
-      cpu    = 0.25
-      memory = "0.5Gi"
-      env {
-        name  = "MONGO_URL"
-        value = azurerm_cosmosdb_account.puntifyeuropemongoaccount.connection_strings[0]
-      }
-
-      env {
-        name  = "SECRET_KEY"
-        value = local.env_variables_europe.SECRET_KEY
-      }
-
-      env {
-        name  = "ALGORITHM"
-        value = local.env_variables_europe.ALGORITHM
-      }
-      env {
-        name  = "ACCESS_TOKEN_EXPIRE_MINUTES"
-        value = local.env_variables_europe.ACCESS_TOKEN_EXPIRE_MINUTES
-
-      }
-    }
-  }
-  ingress {
-    allow_insecure_connections = false
-    external_enabled           = true
-    target_port                = 443
-    traffic_weight {
-      latest_revision = true
-      percentage      = 100
-    }
-  }
-  registry {
-    server               = local.env_variables_europe.DOCKER_REGISTRY_SERVER_URL
-    username             = local.env_variables_europe.DOCKER_REGISTRY_SERVER_USERNAME
-    password_secret_name = "docker-registry-secret"
-  }
-
-  secret {
-    name  = "docker-registry-secret"
-    value = local.env_variables_europe.DOCKER_REGISTRY_SERVER_PASSWORD
-  }
-}
-
-
-resource "azurerm_container_app" "puntifyfronteurope" {
-  name                         = "puntify-front-europe"
-  container_app_environment_id = azurerm_container_app_environment.puntifyeuropeappenvironment.id
-  resource_group_name          = azurerm_resource_group.puntifyeurope.name
-  revision_mode                = "Single"
-  #  latest_revision = true
-  template {
-    container {
-      name   = "puntify-front-europe-container"
-      image  = "acrpuntify.azurecr.io/puntify-front:latest"
-      cpu    = 0.25
-      memory = "0.5Gi"
-      env {
-        name  = "VITE_API_URL"
-        value = "https://${azurerm_container_app.puntifybackeurope.ingress[0].fqdn}"
-      }
-    }
-  }
-  ingress {
-
-    allow_insecure_connections = false
-    external_enabled           = true
-    target_port                = 80
-    traffic_weight {
-      latest_revision = true
-
-      percentage = 100
-    }
-  }
-
-  registry {
-    server               = local.env_variables_europe.DOCKER_REGISTRY_SERVER_URL
-    username             = local.env_variables_europe.DOCKER_REGISTRY_SERVER_USERNAME
-    password_secret_name = "docker-registry-secret"
-  }
-
-  secret {
-    name  = "docker-registry-secret"
-    value = local.env_variables_europe.DOCKER_REGISTRY_SERVER_PASSWORD
-  }
-}
-output "test" {
-  value = "https://${azurerm_container_app.puntifybackeurope.ingress[0].fqdn}"
-}
-resource "azurerm_resource_group" "punifyfrontdoorresourcegroup" {
-  location = "westeurope"
-  name     = "puntify-front-door-resource-group"
-}
-
-
-resource "azurerm_frontdoor" "puntifyfrontdoor" {
-  name                = "puntify"
-  resource_group_name = azurerm_resource_group.punifyfrontdoorresourcegroup.name
-  backend_pool_load_balancing {
-    name = "votingDemoLoadBalancingSettings1"
-
-  }
-
-  backend_pool_health_probe {
-    name     = "votingDemoHealthProbeSetting1"
-    protocol = "Https"
-  }
-  backend_pool {
-    name = "puntify-back-pool"
-
-    backend {
-      address     = azurerm_container_app.puntifyfronteurope.ingress[0].fqdn
-      http_port   = 80
-      https_port  = 443
-      host_header = azurerm_container_app.puntifyfronteurope.ingress[0].fqdn
-    }
-    load_balancing_name = "votingDemoLoadBalancingSettings1"
-    health_probe_name   = "votingDemoHealthProbeSetting1"
-  }
-  routing_rule {
-    name               = "puntify-routing-rule"
-    frontend_endpoints = ["puntify"]
-    accepted_protocols = ["Http", "Https"]
-    patterns_to_match  = ["/*"]
-    forwarding_configuration {
-      forwarding_protocol                   = "HttpsOnly"
-      backend_pool_name                     = "puntify-back-pool"
-      cache_enabled                         = true
-      cache_query_parameter_strip_directive = "StripNone"
-      cache_use_dynamic_compression         = true
-    }
-  }
-  frontend_endpoint {
-    name                         = "puntify"
-    host_name                    = "puntify.azurefd.net"
-    # Update with your custom domain
-    session_affinity_enabled     = false
-    session_affinity_ttl_seconds = 0
-  }
-}
-
-
+#
+#
+#resource "azurerm_container_app" "puntifyfronteurope" {
+#  name                         = "puntify-front-europe"
+#  container_app_environment_id = azurerm_container_app_environment.puntifyeuropeappenvironment.id
+#  resource_group_name          = azurerm_resource_group.puntifyeurope.name
+#  revision_mode                = "Single"
+#  #  latest_revision = true
+#  template {
+#    container {
+#      name   = "puntify-front-europe-container"
+#      image  = "acrpuntify.azurecr.io/puntify-front:latest"
+#      cpu    = 0.25
+#      memory = "0.5Gi"
+#      env {
+#        name  = "VITE_API_URL"
+#        value = "https://${azurerm_container_app.puntifybackeurope.ingress[0].fqdn}"
+#      }
+#    }
+#  }
+#  ingress {
+#
+#    allow_insecure_connections = false
+#    external_enabled           = true
+#    target_port                = 80
+#    traffic_weight {
+#      latest_revision = true
+#
+#      percentage = 100
+#    }
+#  }
+#
+#  registry {
+#    server               = local.env_variables_europe.DOCKER_REGISTRY_SERVER_URL
+#    username             = local.env_variables_europe.DOCKER_REGISTRY_SERVER_USERNAME
+#    password_secret_name = "docker-registry-secret"
+#  }
+#
+#  secret {
+#    name  = "docker-registry-secret"
+#    value = local.env_variables_europe.DOCKER_REGISTRY_SERVER_PASSWORD
+#  }
+#}
+#output "test" {
+#  value = "https://${azurerm_container_app.puntifybackeurope.ingress[0].fqdn}"
+#}
+#resource "azurerm_resource_group" "punifyfrontdoorresourcegroup" {
+#  location = "westeurope"
+#  name     = "puntify-front-door-resource-group"
+#}
+#
+#
+#resource "azurerm_frontdoor" "puntifyfrontdoor" {
+#  name                = "puntify"
+#  resource_group_name = azurerm_resource_group.punifyfrontdoorresourcegroup.name
+#  backend_pool_load_balancing {
+#    name = "votingDemoLoadBalancingSettings1"
+#
+#  }
+#
+#  backend_pool_health_probe {
+#    name     = "votingDemoHealthProbeSetting1"
+#    protocol = "Https"
+#  }
+#  backend_pool {
+#    name = "puntify-back-pool"
+#
+#    backend {
+#      address     = azurerm_container_app.puntifyfronteurope.ingress[0].fqdn
+#      http_port   = 80
+#      https_port  = 443
+#      host_header = azurerm_container_app.puntifyfronteurope.ingress[0].fqdn
+#    }
+#    load_balancing_name = "votingDemoLoadBalancingSettings1"
+#    health_probe_name   = "votingDemoHealthProbeSetting1"
+#  }
+#  routing_rule {
+#    name               = "puntify-routing-rule"
+#    frontend_endpoints = ["puntify"]
+#    accepted_protocols = ["Http", "Https"]
+#    patterns_to_match  = ["/*"]
+#    forwarding_configuration {
+#      forwarding_protocol                   = "HttpsOnly"
+#      backend_pool_name                     = "puntify-back-pool"
+#      cache_enabled                         = true
+#      cache_query_parameter_strip_directive = "StripNone"
+#      cache_use_dynamic_compression         = true
+#    }
+#  }
+#  frontend_endpoint {
+#    name                         = "puntify"
+#    host_name                    = "puntify.azurefd.net"
+#    # Update with your custom domain
+#    session_affinity_enabled     = false
+#    session_affinity_ttl_seconds = 0
+#  }
+#}
+#
+#
